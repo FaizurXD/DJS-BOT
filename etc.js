8dd1c7d1f942a97247a1f6ff87d3c86642ae5f05
Faizur <166435454+FaizurXD@users.noreply.github.com>
2024-05-07 02:33:05 +0530

    Delete etc.js

--- etc.js
+++
@@ -1,7 +0,0 @@
-const filesToRequire = ['./pinger247.js', './alive.js', './animation.js'];
-
-filesToRequire.forEach(file => {
-    require(file);
-});
-
-console.log("All of your required JS files are started.");

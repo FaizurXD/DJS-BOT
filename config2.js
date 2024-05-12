627bf575c3e297f880838a72989d4a602ddca42f
Faizur <166435454+FaizurXD@users.noreply.github.com>
2024-05-12 22:07:44 +0530

    Create config2.js

---
+++ config2.js
@@ -0,0 +1,39 @@
+{
+	"clientID" : "1115376371254972478",
+	"exitOnDiscordClose": false,
+	"UsingProgram": true,
+	"assetsCycle" : true,
+	"stateCycle" : true,
+	"assets": [
+		{
+			"name": "By Faizur",
+			"quote": " contact @Faizur_ for custom bots"
+		},
+		{
+			"name": "IMMORTAL NETWORK",
+			"quote": "Best Minecraft Server"
+		},
+		{
+			"name": "ILLEGAL XD NAGAR",
+			"quote": "THE HIDDEN NAGAR"
+		}
+	],
+	"state" : [
+		"| BY Faizur",
+		"Joining Immortal network...",
+		"illeagl xd nagar",
+		"Entering The realm of immortal network...",
+		" Conatct @Faizur_ For custom bots"
+	],
+  	"largeImageText" : "Faizur - The Creator of this bot",
+	"buttons": [
+		{
+			"label" : "IMMORTAL NETWORK",
+			"url" : "https://discord.com/invite/9YZbNP3jJk"
+		},
+		{
+			"label" : "ILLEGAL XD NAGAR", 
+			"url" : "https://discord.com/invite/kDQ7yjdscF"
+		}
+	]
+}

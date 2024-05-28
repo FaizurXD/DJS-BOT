91cc6c33bfe2b103fb487f89c4be463cc06907f9
Faizur <166435454+FaizurXD@users.noreply.github.com>
2024-05-28 16:13:53 +0530

    Update pinger247.js

--- pinger247.js
+++ pinger247.js
@@ -1,12 +1,16 @@
-function pingWebsite() {
-  fetch('https://illegal-faizur.onrender.com')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      console.log('Website pinged successfully');
-    })
-    .catch(error => console.error('There was a problem with the ping:', error));
+function pingWebsites() {
+  const websites = ['https://illegal-faizur.onrender.com', 'https://aioo-3a2a.onrender.com'];
+
+  websites.forEach(website => {
+    fetch(website)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        console.log(`Website ${website} pinged successfully`);
+      })
+      .catch(error => console.error(`There was a problem with the ping for ${website}:`, error));
+  });
 }
 
-setInterval(pingWebsite, 30000);
+setInterval(pingWebsites, 30000);

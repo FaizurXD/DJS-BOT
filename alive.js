d63ba7524cb9634581be9f191e46171617d24ce1
Faizur <166435454+FaizurXD@users.noreply.github.com>
2024-06-10 01:18:29 +0530

    Update alive.js

--- alive.js
+++ alive.js
@@ -7,13 +7,16 @@ const targetUrl = "minecraft://?addExternalServer=ELECTRIC NETWORK|fun.minecraft
 // Create a simple HTTP server
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
+  console.log(`Received request for ${parsedUrl.pathname}`); // Log the pathname
   
   // Check if the route matches /api/mc-server/Electric
   if (parsedUrl.pathname === "/api/mc-server/Electric") {
+    console.log("Redirecting to target URL"); // Log redirection
     // Redirect to the target URL
     res.writeHead(302, { "Location": targetUrl });
     res.end();
   } else {
+    console.log("Sending default response"); // Log default response
     // Handle other routes or show a default message
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Bot started!");
